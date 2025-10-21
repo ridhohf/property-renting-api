7ebe43f3d7281204ad7efbe333f3a585f90ebcfb
Ridho <ridhohanafi2000@gmail.com>
2025-10-21 16:45:25 +0700

    fix: config

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,7 @@
 import { config } from "dotenv";
 import { resolve } from "path";
 
-export const NODE_ENV = process.env.NODE_ENV || "development";
-const envFile = NODE_ENV === "development" ? ".env.development" : ".env";
-
-config({ path: resolve(__dirname, `../../${envFile}`) });
-config({ path: resolve(__dirname, `../../${envFile}.local`), override: true });
+config({ path: resolve(__dirname, "../.env") });
 
 export const PORT = process.env.PORT || 8000;
 export const DATABASE_URL = process.env.DATABASE_URL || "";
@@ -21,7 +17,10 @@ export const SMTP_HOST = process.env.SMTP_HOST || "";
 export const SMTP_PORT = parseInt(process.env.SMTP_PORT || "587");
 export const SMTP_USER = process.env.SMTP_USER || "";
 export const SMTP_PASSWORD = process.env.SMTP_PASSWORD || "";
-export const EMAIL_FROM = process.env.EMAIL_FROM || "ridhohanafi2000@gmail.com";
+export const EMAIL_FROM = process.env.EMAIL_FROM || "twiceoncess2015@gmail.com";
 
 export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:3000";
 export const OPENCAGE_API_KEY = process.env.OPENCAGE_API_KEY || "";
+
+
+
